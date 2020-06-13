876b1ff0bed2f87547fcf5d68052ae02ff070013
rashu <rashuzak2@gmail.com>
2020-06-13 12:31:07 -0500

    Updates

---
+++ precache-manifest.35b399007d502f2d68c20df811b01c5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b5692b6399db17c4093db282fc7df3",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "f8350d6f51e86a4c053b",
+    "url": "/clicky-game/static/css/main.f1e0bacf.chunk.css"
+  },
+  {
+    "revision": "a1f037ede2c8bd40ca86",
+    "url": "/clicky-game/static/js/2.ba8a0e31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.ba8a0e31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8350d6f51e86a4c053b",
+    "url": "/clicky-game/static/js/main.f8264306.chunk.js"
+  },
+  {
+    "revision": "1c01b86330b9c0bba0c4",
+    "url": "/clicky-game/static/js/runtime-main.c00330a8.js"
+  }
+]);
